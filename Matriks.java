f8fe3ab5e616690b92261a05d0958c4682ec6560
fwznbg <fnaabigh@gmail.com>
2020-09-30 23:40:50 +0700

    bikin main sama gabungin

--- Matriks.java
+++ Matriks.java
@@ -396,8 +396,8 @@ public class Matriks {
             }
         }
         
-        //tinggal nunggu fungsi gaussnya
-        intpls.tulisMatriks();
+        intpls = intpls.gauss(intpls);
+        
     }
 
     void regresi () {
@@ -430,9 +430,44 @@ public class Matriks {
         M.tulisMatriks();  
     }
     }
+
+    Matriks gauss(Matriks M) {
+        int i,j;
+        i=0;
+        j=0;
+        for (i=0;i<=M.getLastIdxBrs();i++){ //iterasi tiap baris
+            int a=i;
+            int b=i;
+            boolean tertukar=false;
+            while (a<=M.getLastIdxKlm() && !tertukar){
+                while(b<=M.getLastIdxBrs() && M.isNol(b, a)){
+                    b++;
+                }
+                if (M.isNol(b, a)==false){
+                    M.tukarBaris(i, b);
+                    tertukar=true;
+                }
+                else{
+                    a++;
+                }
+            }
+            j=M.idxNotNol(i); //mencari elemen pertama tidak 0 pada suatu baris
+            M.kaliSkalar(i, +(1/M.getIsi(i, j))); //mengubah elemen menjadi lead eselon
+            for(int c=i+1;c<=M.getLastIdxBrs();c++){ //menjadikan setiap baris pada kolom j menjadi 0
+                M.kurangBaris(c, i, M.getIsi(c, j));
+            }
+        }
+        for (i=0;i<=M.getLastIdxBrs();i++){
+            for (j=0;j<=M.getLastIdxBrs();j++){
+                M.setIsi(i, j, M.getIsi(i, j)+0.0);
+            }
+        }
+        return M;   
+    }
     public static void main(String[] args){
         Matriks matriks1 = new Matriks(3, 4);
         matriks1.bacaMatriks();
-        matriks1.splinvers(matriks1);
+        matriks1 = matriks1.gauss(matriks1);
+        matriks1.tulisMatriks();
     }
 }
