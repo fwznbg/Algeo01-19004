bae15986ffb29268ea9b9777c3276ae435c84102
bintangpananjung <pananjungbintang@gmail.com>
2020-10-01 21:25:47 +0700

    nambahin solvegauss(blm fix) + revisi gauss

--- src/Gauss.java
+++ src/Gauss.java
@@ -8,25 +8,31 @@ public class Gauss {
         j=0;
         for (i=0;i<=M.getLastIdxBrs();i++){ //iterasi tiap baris
             int a=i;
-            int b=i;
+            boolean z=true;
             boolean tertukar=false;
             while (a<=M.getLastIdxKlm() && !tertukar){
+                int b=i;
                 while(b<=M.getLastIdxBrs() && M.isNol(b, a)){
+                    z=M.isNol(b, a);
                     b++;
                 }
-                if (M.isNol(b, a)==false){
-                    M.tukarBaris(i, b);
-                    tertukar=true;
-                }
-                else{
-                    a++;
+                if(b<=M.getLastIdxBrs())
+                    z=M.isNol(b, a);
+                    if (z==false){
+                        M.tukarBaris(i, b);
+                        tertukar=true;
+                    }
+                a++;
+            }
+            if (!M.isBarisNol(i) || !M.isNoSolution(i)){
+                j=M.idxNotNol(i); //mencari elemen pertama tidak 0 pada suatu baris
+                M.kaliSkalar(i, +(1/M.getIsi(i, j))); //mengubah elemen menjadi lead eselon
+                
+                for(int c=i+1;c<=M.getLastIdxBrs();c++){ //menjadikan setiap baris pada kolom j menjadi 0
+                    M.kurangBaris(c, i, M.getIsi(c, j));
                 }
             }
-            j=M.idxNotNol(i); //mencari elemen pertama tidak 0 pada suatu baris
-            M.kaliSkalar(i, +(1/M.getIsi(i, j))); //mengubah elemen menjadi lead eselon
-            for(int c=i+1;c<=M.getLastIdxBrs();c++){ //menjadikan setiap baris pada kolom j menjadi 0
-                M.kurangBaris(c, i, M.getIsi(c, j));
-            }
+
         }
         for (i=0;i<=M.getLastIdxBrs();i++){
             for (j=0;j<=M.getLastIdxBrs();j++){
@@ -37,7 +43,7 @@ public class Gauss {
     }
     public static void main(String[] args){
         Gauss g= new Gauss();
-        Matriks M=new Matriks(4,5);
+        Matriks M=new Matriks(3,4);
         M.bacaMatriks();
         g.gauss(M).tulisMatriks();
     }
